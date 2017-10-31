67430d33a0887b09092e20d9350813d2a1141d2d
Ashe Austaire <AsheAustaire@gmail.com>
2017-10-31 14:18:00 -0400

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,18 @@
-// Code EyesOnMe Component Here
+import React, {Component} from 'react';
+
+export default class EyesOnMe extends Component {
+
+  focus = function(){
+    console.log('Good!')
+  }
+
+  blur = function(){
+    console.log('Hey! Eyes on me!')
+  }
+
+  render(){
+    return (
+      <button onFocus={this.focus} onBlur={this.blur}>LOOK OVER HERE</button>
+    )
+  }
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,12 @@
-// Code Keypad Component Here
+import React, { Component } from 'react';
+
+
+
+export default class Keypad extends Component {
+
+  tickle = function(){console.log('Entering password...')}
+
+  render(){
+    return <input onKeyUp={this.tickle} type="password" />
+  }
+}
